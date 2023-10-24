c311e02a4327181d918872c7f2d01089071a0bb3
umer <54628763+TheUmerator@users.noreply.github.com>
2023-10-24 07:48:46 +0500

    Fill out 90% of the menu items excluding desserts

--- src/index.js
+++ src/index.js
@@ -14,16 +14,32 @@ selectTab.forEach((tab) => {
         const currentTab = tab.classList[0];
         console.log(currentTab);
 
+        // if(tab.classList[2]=='selected'){
+        //     tab.classList
+        // }
+
         switch (currentTab) {
             case 'menu':
                 console.log('1');
-                
+                tab.classList.add('selected');
+                // tab.classList.remove('selected');
+                // Clear content
+                // Add new respective content
+                // tab.classList.add('selected');
                 break;
             case 'res':
                 console.log('2');
+                // tab.classList.remove('selected');
+                // Clear content
+                // Add new respective content
+                // tab.classList.add('selected');
                 break;
             case 'about':
                 console.log('3');
+                // tab.classList.remove('selected');
+                // Clear content
+                // Add new respective content
+                // tab.classList.add('selected');
                 break;
         }
     })
---
+++ src/menu.js
@@ -0,0 +1,17 @@
+export function loadMenu(){
+    let mealTypes=['Breakfast','Brunch','Dinner','Dessert'];
+    
+    let breakItems= ["Sydney's Omlette","Longanisa Breakfast Sandwich with Hash Browns"];
+    let brunchItems=["Focaccia","Braised Beef Sandwiches"];
+    let dinnerItems=["Chicago-Style Deep Dish Pizza","Carmy's Lemon Chicken Piccata","Mikey's Beef Braciole"];
+    let dessItems=  ["Marcus's Chocolate Ganache Cake","Marcus's Perfect Doughnut","Carmy's Plum Gelée"];
+
+    let breakDesc=  ["A delicate French omelette filled with Boursin cheese and topped with crushed sour cream and onion potato chips.",
+                    "Filipino-inspired breakfast sandwich with savory longanisa sausage, fried egg, melted American cheese, and crispy hash browns, on toasted English muffins"];
+    let brunchDesc= ["Italian flatbread with a golden brown crust and soft interior. Typically topped with herbs, olive oil, and vegetables.",
+                    "Chicago-style Italian beef sandwiches made with tender and juicy beef, sweet or hot peppers, giardiniera, and jus."];
+    let dinnerDesc= ["Thick crust pizza with generous amounts of cheese, meat, vegetables, and tomato sauce. Baked at a high temperature to create a crispy crust and moist interior.",
+                    "Tender chicken cutlets in a bright and flavorful lemon butter sauce, accented with briny capers and fresh parsley.",
+                    "Thinly sliced beef layered with prosciutto, breadcrumbs, cheese, pine nuts, and parsley, then rolled up and braised in a flavorful tomato sauce. Served over your choice of pasta."];
+    let dessDesc=   ["A rich and decadent chocolate cake layered with silky smooth ganache frosting. ","",""];
+}
