41cc091bf19dc82662ec4d24c24a6746082fdce3
umer <54628763+TheUmerator@users.noreply.github.com>
2023-10-25 06:50:27 +0500

    Add all the text content in menu items

--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,25 @@
-export function loadMenu(){
-    let mealTypes=['Breakfast','Brunch','Dinner','Dessert'];
-    
-    let breakItems= ["Sydney's Omlette","Longanisa Breakfast Sandwich with Hash Browns"];
-    let brunchItems=["Focaccia","Braised Beef Sandwiches"];
-    let dinnerItems=["Chicago-Style Deep Dish Pizza","Carmy's Lemon Chicken Piccata","Mikey's Beef Braciole"];
-    let dessItems=  ["Marcus's Chocolate Ganache Cake","Marcus's Perfect Doughnut","Carmy's Plum Gelée"];
+export function loadMenu() {
+    let mealTypes = ['Breakfast', 'Brunch', 'Dinner', 'Dessert'];
 
-    let breakDesc=  ["A delicate French omelette filled with Boursin cheese and topped with crushed sour cream and onion potato chips.",
-                    "Filipino-inspired breakfast sandwich with savory longanisa sausage, fried egg, melted American cheese, and crispy hash browns, on toasted English muffins"];
-    let brunchDesc= ["Italian flatbread with a golden brown crust and soft interior. Typically topped with herbs, olive oil, and vegetables.",
-                    "Chicago-style Italian beef sandwiches made with tender and juicy beef, sweet or hot peppers, giardiniera, and jus."];
-    let dinnerDesc= ["Thick crust pizza with generous amounts of cheese, meat, vegetables, and tomato sauce. Baked at a high temperature to create a crispy crust and moist interior.",
-                    "Tender chicken cutlets in a bright and flavorful lemon butter sauce, accented with briny capers and fresh parsley.",
-                    "Thinly sliced beef layered with prosciutto, breadcrumbs, cheese, pine nuts, and parsley, then rolled up and braised in a flavorful tomato sauce. Served over your choice of pasta."];
-    let dessDesc=   ["A rich and decadent chocolate cake layered with silky smooth ganache frosting. ","",""];
-}
+    let breakItems = ["Sydney's Omlette", "Longanisa Breakfast Sandwich with Hash Browns"];
+    let brunchItems = ["Focaccia", "Braised Beef Sandwiches"];
+    let dinnerItems = ["Chicago-Style Deep Dish Pizza", "Carmy's Lemon Chicken Piccata", "Mikey's Beef Braciole"];
+    let dessItems = ["Marcus's Chocolate Ganache Cake", "Marcus's Perfect Doughnut", "Carmy's Plum Gelée"];
+
+    let breakDesc = ["A delicate French omelette filled with Boursin cheese and topped with crushed sour cream and onion potato chips.",
+        "Filipino-inspired breakfast sandwich with savory longanisa sausage, fried egg, melted American cheese, and crispy hash browns, on toasted English muffins"
+    ];
+    let brunchDesc = ["Italian flatbread with a golden brown crust and soft interior. Typically topped with herbs, olive oil, and vegetables.",
+        "Chicago-style Italian beef sandwiches made with tender and juicy beef, sweet or hot peppers, giardiniera, and jus."
+    ];
+    let dinnerDesc = ["Thick crust pizza with generous amounts of cheese, meat, vegetables, and tomato sauce. Baked at a high temperature to create a crispy crust and moist interior.",
+        "Tender chicken cutlets in a bright and flavorful lemon butter sauce, accented with briny capers and fresh parsley.",
+        "Thinly sliced beef layered with prosciutto, breadcrumbs, cheese, pine nuts, and parsley, then rolled up and braised in a flavorful tomato sauce. Served over your choice of pasta."
+    ];
+    let dessDesc = ["A rich and decadent chocolate cake layered with silky smooth ganache frosting. ", 
+        "The classic American dessert elevated with premium ingredients and house-made chocolate glaze. Served with chocolate mousse and fresh raspberries", 
+        "A sweet and tart plum jelly with a chewy texture, served with compressed plums and plum wine syrup."];
+
+    // let mealRotation=[mealTypes][breakItems];
+
+    }
